7481946b6fe1b1b243a9b8e99a87712e2263228e
Liam Kirkland <32485432+LiamKirkland@users.noreply.github.com>
2026-07-20 11:48:04 -0400

    Update 'hola' spec to look for correct output

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -15,7 +15,7 @@ describe "Rakefile" do
 
     describe 'greeting:hola' do
       it "should print out 'hola desde Rake!'" do
-        expect($stdout).to receive(:puts).with("hola de Rake!")
+        expect($stdout).to receive(:puts).with("hola desde Rake!")
         Rake::Task["greeting:hola"].invoke
       end
     end
